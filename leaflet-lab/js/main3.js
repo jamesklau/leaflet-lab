bbbdc9eb02150ed5d42d2ab27457d1145c64a354
jamesklau <46978880+jamesklau@users.noreply.github.com>
2019-02-23 14:45:09 -0600

    Add files via upload
    Module 5A Complete

---
+++ leaflet-lab/js/main3.js
@@ -0,0 +1,144 @@
+/* Map of GeoJSON data from MegaCities.geojson */
+
+//function to instantiate the Leaflet map
+function createMap(){
+    //create the map
+    var map = L.map('map', {
+        center: [20, 0],
+        zoom: 2
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+    
+    getData(map);
+};
+
+//function to attach popups to each mapped feature
+function onEachFeature(feature, layer) {
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = 50;
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
+
+    return radius;
+};
+
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = "Pop_2015";
+
+    //create marker options
+    var options = {
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+        //original popupContent changed to panelContent...Example 2.2 line 1
+    var panelContent = "<p><b>City:</b> " + feature.properties.City + "</p>";
+
+    //add formatted attribute to panel content string
+    var year = attribute.split("_")[1];
+    panelContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + " million</p>";
+
+    //popup content is now just the city name
+    var popupContent = feature.properties.City;
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius),
+        closeButton: false 
+    });
+
+    //event listeners to open popup on hover and fill panel on click
+    layer.on({
+        mouseover: function(){
+            this.openPopup();
+        },
+        mouseout: function(){
+            this.closePopup();
+        },
+        click: function(){
+            $("#panel").html(panelContent);
+        }
+    });
+    
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(data, map){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: pointToLayer
+    }).addTo(map);
+};
+//function to retrieve the data and place it on the map
+function getData(map){
+     //load the data
+    $.ajax("data/MegaCities.geojson", {
+        dataType: "json",
+        success: function(response){
+            //call function to create proportional symbols
+            createPropSymbols(response, map);
+        }
+    });
+};
+
+//function that creates the orange circles for placemarkers
+function getData2(map){
+    //load the data
+    $.ajax("data/MegaCities.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
+            }).addTo(map);
+        }
+    });
+};
+
+$(document).ready(createMap);
